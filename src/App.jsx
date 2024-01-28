f2c692ae73725a880cdf64294e07fcebebd1fc55
nagvanshi9275 <106686974+nagvanshi9275@users.noreply.github.com>
2024-01-28 10:32:50 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+
+
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="bg-slat-900 grid place-content-center h-screen">
+
+
+      <img className="w-316 md:w-132 lg:w-148" src="https://i.postimg.cc/HkZ3RF4d/pexels-samer-daboul-1252812.jpg"/>
+
+
+
+        
+      <div className="bg-red-800 w-700 h-600 mt-6 flex flex-col ">
+
+
+       <h1>kon</h1>
+
+
+
+
+       <p>loshsd sdsd scsd sdcdsf wefhsdc sdhwed ewdsc sdicjas axdass adxha sSOIQJS QSIWSD WIUDHQ AAUXHSD</p>
+
+
+      <img src="https://i.postimg.cc/gcDQztGL/Fifa-2023.jpg"/>
+
+      <p>ms dhoni is the world best cricketer </p>
+
+
+
+
+      </div>
+
+
+
+
+
+        
+
+
+
+
+
+      </div>
+
+
+
+    
+
+
+
+    </>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
